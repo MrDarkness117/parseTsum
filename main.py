4b79e97b8cb0ab58193b10671aa5c6fdf2e354b1
Михаил Романцов <romantsov@noone.office.local>
2021-11-09 10:37:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from os import path
 import re
@@ -20,13 +20,42 @@ driver = webdriver.Chrome(options=options)
 driver.maximize_window()
 driver.implicitly_wait(6)
 
-url_f =     "https://www.tsum.ru/catalog/obuv-18405/?availability=2&page=1"
-url_f2 =    "https://www.tsum.ru/catalog/obuv-18405/?availability=2&page=101"
-url_f3 =    "https://www.tsum.ru/catalog/obuv-18405/?availability=2&page=201"
-url_f4 =    "https://www.tsum.ru/catalog/obuv-18405/?availability=2&page=301"
-url_m =     "https://www.tsum.ru/catalog/obuv-18440/?availability=2&page=1"
-url_f_k =   "https://www.tsum.ru/catalog/obuv_dlya_devochek-3442/"
-url_m_k =   "https://www.tsum.ru/catalog/obuv_dlya_malchikov-3425/"
+url_f = "https://www.tsum.ru/catalog/obuv-18405/?availability=2&page=1"
+url_f_sumki = 'https://www.tsum.ru/catalog/sumki-18438/'
+url_f_aksess = 'https://www.tsum.ru/catalog/aksessuary-2404/'
+url_f_modded = "https://www.tsum.ru/catalog/obuv-18405/?availability=2&page=289"
+url_f_f_wd = 'https://www.tsum.ru/catalog/obuv-18405/?brand=10468094&availability=2'
+url_f_f_wd_av = 'https://www.tsum.ru/catalog/obuv-18405/?brand=10468094'
+url_f_le_silla_av = 'https://www.tsum.ru/catalog/obuv-18405/?brand=448697'
+url_m = "https://www.tsum.ru/catalog/obuv-18440/?availability=2&page=1"
+url_m_sumki = 'https://www.tsum.ru/catalog/muzhskie_sumki-4051/'
+url_m_aksess = 'https://www.tsum.ru/catalog/aksessuary-4031/'
+url_m_modded = "https://www.tsum.ru/catalog/obuv-18440/?availability=2&page=203"
+url_f_k = "https://www.tsum.ru/catalog/obuv_dlya_devochek-3442/"
+url_m_k = "https://www.tsum.ru/catalog/obuv_dlya_malchikov-3425/"
+
+url_brands = "https://www.tsum.ru/brand/"
+
+brands = [
+    "Aldo Brue", "AGL", "BANU", "Bally", 'Bresciani', 'Brimarts', 'Carlo Visintini', 'Casadei', 'Casheart,',
+    'Cerruti 1881', 'Cesare Casadei', 'Coccinelle', 'DKNY', 'Doria Maria', 'Doucal\'s', 'F_WD', 'Fabi', 'Fabrizio Lesi',
+    'Ferre Milano', 'Flower Mountain', 'Franceschetti', 'Frasconi', 'Fratelli Rossetti', 'Fratelli Rossetti One',
+    'Gianni Chiarini', 'Goose Tech', 'GUM', 'HIDE&JACK', 'Ice Play', 'Iceberg', 'In The Box', 'Inuikii',
+    'John Galliano', 'John Richmond', 'Kalliste', 'Kat Maconie', 'Kate Spade', 'Lancaster', 'Landi', 'Le Silla',
+    'Lemon Jelly', "L'Impermeabile", 'Marsell', 'Merola Gloves', 'Moose Knuckles', 'Moreschi', 'Moschino', 'Panchic',
+    'Pantanetti', 'Parajumpers', 'Pasotti', 'Pertini', 'Pierre Cardin', 'Pollini', 'Portolano', 'Premiata',
+    'Principe Di Bologna', 'RBRSL', "Reptile's House", 'Roberto Cavalli', 'Rocco P', 'Sergio Rossi', 'SPRAYGROUND',
+    'Stemar', 'Stuart Weitzman', 'V SEASON', "VIC MATIE'", 'Voile Blanche', 'What For', 'Walford', '3JUIN',
+    'Premiata will be', 'Sprayground'
+]
+
+search_values = ['Wolford', 'RBRSL']
+
+categories = [
+    # 'Мужское',
+    # "Женское",
+    "Детское"
+]
 
 present = '//*[@id="catalog"]/div[2]/div/div/div/div[1]/filter-desktop/div[5]/div/div/div/div/div[3]' \
           '/div/perfect-scrollbar/div/div[1]/div/div[1]/div/label'
@@ -37,6 +66,52 @@ failed_pages = {'pages': []}
 
 tables = {}
 count = 0
+closed = False
+
+
+def open_brands():
+    driver.get(url_brands)
+    for c in categories:
+        try:
+            driver.find_element_by_xpath('//div[@class="header__gender-switch header__gender-switch_desktop"]//span[contains(text(), "{}")]'.format(c)).click()
+        except:
+            print('Category ' + c + ' already open')
+        for el in brands:
+            driver.get(url_brands)
+            try:
+                driver.execute_script('window.scrollBy(0, 7000)')
+                time.sleep(1)
+                WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+                    (By.XPATH, '//span[@class="brand-list__full-show"]')
+                ))
+                driver.find_element_by_xpath('//span[@class="brand-list__full-show"]').click()
+                time.sleep(2)
+                driver.execute_script('window.scrollBy(0, 7000)')
+                time.sleep(1)
+                driver.execute_script('window.scrollBy(0, 7000)')
+                time.sleep(1)
+                driver.execute_script('window.scrollBy(0, 7000)')
+                time.sleep(1)
+                driver.execute_script('window.scrollBy(0, 7000)')
+                driver.find_element_by_xpath('//span[contains(text(), "{}")]'.format(el)).click()
+                write_data()
+            except Exception as e:
+                print(el + " not found in the list, skipping.")
+
+
+def search():
+    driver.get(url_brands)
+    for b in search_values:
+        # WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//button[@class="search-mobile__open-button"]')))
+        driver.find_element_by_xpath('//div[@class="header__search-form"]').click()
+        driver.find_element_by_xpath('//input[@class="field__input"]').clear()
+        driver.find_element_by_xpath('//input[@class="field__input"]').send_keys(b)
+        driver.find_element_by_xpath('//input[@class="field__input"]').send_keys(Keys.ENTER)
+        time.sleep(2)
+        try:
+            write_data()
+        except Exception as e:
+            print("Failure in finding elements")
 
 
 def change_page():
@@ -84,6 +159,7 @@ def change_page():
 
 
 def get_data():
+    # driver.execute_script('window.scrollBy(0, -7000)')
     print('Get prices')
     elems_var = '//div[@class="product-list__products ng-star-inserted"]/div'
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, elems_var)))
@@ -93,10 +169,13 @@ def get_data():
     )))
     counter = 0
     print('[begin gather loop]')
-    time.sleep(3)
+    print(elems)
     for el in elems:
         counter += 1
+        driver.execute_script('window.scrollBy(0, {})'.format(counter * 20))
         try:
+            title = str(el.find_element_by_xpath('//div[@class="product-list__products ng-star-inserted"]'
+                                                          '/div[{}]/div/div/a/*[1]'.format(counter)).get_attribute('title'))
             section_base = '//div[@class="product-list__products ng-star-inserted"]/div[{}]'.format(counter)
             # print(section_base)
             for bit in [
@@ -115,75 +194,48 @@ def get_data():
                                              'a/p[@class="product__brand"]'.format(counter)).text
             link = el.find_element_by_xpath('//div[@class="product-list__products ng-star-inserted"]/div[{}]'
                                             '/div/div/a'.format(counter)).get_attribute('href')
+            description = el.find_element_by_xpath('//div[@class="product-list__products ng-star-inserted"]/div[{}]'
+                                            '//p[@class="product__description"]'.format(counter)).text
+            article_brand = ' '.join(
+                ' '.join(title.replace(description.lower() + ' ', '').replace(brand + ' ', "").split(' ')[5:]).replace("руб., арт. ", "")
+                .replace("| ", "").replace(" Фото 1", "").replace("Фото", "").replace("арт. ", "").replace("цвета ", "")
+                .replace("цене ", "").replace(price, "").replace('по ', "").replace("бежевого ", "").replace("черного ", "")
+                .replace("серого ", '').replace("белого ", "").replace("темно-синего ", "").replace("хаки ", "")
+                .replace("серебряного ", "").replace("синего ", "").replace("розового ", "").replace("золотого ", "")
+                .replace("коричневого ", "").split(' '))
+            if article_brand[0] == ' ': article_brand = article_brand[1:]
+            # print(article_brand)
 
-            tables[article] = [price, brand, link]
+            tables[article] = [price, brand, article_brand, link]
         except Exception as e:
             print("Exception detected: " + str(e))
             global failed_pages
             failed_pages['pages'].append(re.sub('[^0-9]', '', str(driver.current_url)[-3:]).replace('=', ''))
-            """if driver.current_url == 'https://www.tsum.ru/catalog/obuv-18405/?availability=2':
-                driver.refresh()
-                time.sleep(4)
-                article = re.sub("[^0-9]", '', el.find_element_by_xpath(
-                    '//div[@class="product-list__products ng-star-inserted"]/div[{}]/div//a[@class="product__info"]'.format(counter)).get_attribute('href'))[:7]
-                price = re.sub("[^0-9]", '', el.find_element_by_xpath(
-                    '//div[@class="product-list__products ng-star-inserted"]/div[{}]'
-                    '//span[@class="product__price-wrapper"]/span/span/span'.format(counter)).text)
-                brand = el.find_element_by_xpath('//div[@class="product-list__products ng-star-inserted"]/div[{}]/div//'
-                                                 'a/p[@class="product__brand"]'.format(counter)).text
-                link = el.find_element_by_xpath('//div[@class="product-list__products ng-star-inserted"]/div[{}]'
-                                                '/div/div//a[@class="product__info"]'.format(counter)).get_attribute('href')
-
-                tables[article] = [price, brand, link]"""
-            # print('Reloading page...')
-            # driver.refresh()
-            # time.sleep(3)
-            # WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, el.find_element_by_xpath(
-            # '//div[@class="product-list__products ng-star-inserted"]/div[1]'
-            # '/div/div/a'))))
-            # try:
-            #     article = re.sub("[^0-9]", '', el.find_element_by_xpath(
-            #         '//div[@class="product-list__products ng-star-inserted"]/div[{}]'
-            #         '/div/div/a'.format(counter)).get_attribute('href'))[:7]
-            #     price = re.sub("[^0-9]", '', el.find_element_by_xpath(
-            #         '//div[@class="product-list__products ng-star-inserted"]/div[{}]'
-            #         '//span[@class="product__price-wrapper"]/span/span/span'.format(counter)).text)
-            #     brand = el.find_element_by_xpath('//div[@class="product-list__products ng-star-inserted"]/div[{}]/div//'
-            #                                      'a/p[@class="product__brand"]'.format(counter)).text
-            #     link = el.find_element_by_xpath('//div[@class="product-list__products ng-star-inserted"]/div[{}]'
-            #                                     '/div/div/a'.format(counter)).get_attribute('href')
-            #
-            #     tables[article] = [price, brand, link]
-            # except Exception as e:
-            #     print("Exception detected: " + str(e))
-            #     global failed_pages
-            #     failed_pages['pages'].append(re.sub('[^0-9]', '', str(driver.current_url)[-3:]).replace('=', ''))
-
-
     # print("Total: \n" + str(tables))
     print("Page {}".format(str(re.sub('[^0-9]', '', str(driver.current_url)[-3:]).replace('=', ''))))
     print('Prices obtained')
 
 
-def write_data(url):
-    driver.get(url)
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, last_page)))
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    while driver.find_element_by_xpath(last_page).get_attribute('class') != pagination_class_selected:
+def write_data():
+    try:
+        while driver.find_element_by_xpath(last_page).get_attribute('class') != pagination_class_selected:
+            get_data()
+            change_page()
+            # time.sleep(2)
+    except:
         get_data()
-        change_page()
-        # try:
-        #     if int(re.sub('[^0-9]', '', str(driver.current_url)[-3:]).replace('=', '')) % 100 == 0:
-        #         break
-        # except Exception as e:
-        #     print("No page number, skipping: " + str(e))
-        time.sleep(2)
-    print(tables)
+    # print(tables)
 
 
 def write_file(url, filename):
     try:
-        write_data(url)
+        """ ==== FULL ==== """
+        # driver.get(url)
+        # write_data()
+        """ ==== BRANDS ==== """
+        # open_brands()
+        """ ==== SEARCH ==== """
+        search()
     except Exception as e:
         print("Error caught, terminating: " + str(e))
     print('Writing file...')
@@ -200,15 +252,22 @@ def write_file(url, filename):
         print('...Completed writing')
         t.close()
 
-    with open('failed_pages.json', 'w', encoding='utf-8') as p:
+    with open('{}_failed_pages.json'.format(filename), 'w', encoding='utf-8') as p:
+
         json.dump(failed_pages, p, ensure_ascii=False, indent=4)
         p.close()
 
 
 # write_file(url_m, 'obuv_m')
-write_file(url_f, 'obuv_f')
-# write_file(url_f2, 'obuv_f')
-# write_file(url_f3, 'obuv_f')
+# write_file(url_f, 'obuv_f')
+# write_file(url_f_modded, 'obuv_f_modded')
+# write_file(url_f_f_wd_av, 'obuv_f_f_wd_av')
+# write_file(url_f_le_silla_av, 'obuv_f_le_silla_av')
+# write_file(url_f_modded, 'obuv_f_modded_3')
+# write_file(url_m_modded, 'obuv_m_modded')
 # write_file(url_f4, 'obuv_f')
+# write_file(url_f_sumki, 'sumki_f')
+# write_file(url_f_aksess, 'aksessuari_f')
+write_file(url_brands, 'brands_search_wolford_rbrsl')
 
 print("End: " + str(datetime.datetime.now()))
